ad2a5a65dbeb95a3f8b0446b4f850104f2b8d596
Prince <princek2530@gmail.com>
2022-12-19 06:22:39 +0530

    Added: Ability to delete a person using his first name

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -4,10 +4,7 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program in AddressBookMain class on Main Branch");
         AddressBook addressBook = new AddressBook();
-        addressBook.addContact();
-        System.out.println(addressBook);
-        addressBook.editContact();
-        System.out.println(addressBook);
+        addressBook.mainMenu();
         System.out.println("Thanks for using Address Book 📝");
     }
 }
